0717ca12133561a626233bcec1514c16a1846e32
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-05-02 22:28:18 +0900

    feat: UserService에 join() 메서드 구현

--- src/main/java/com/cloud/SubwayChat/service/UserService.java
+++ src/main/java/com/cloud/SubwayChat/service/UserService.java
@@ -1,7 +1,23 @@
 package com.cloud.SubwayChat.service;
 
+import com.cloud.SubwayChat.domain.SubwayLine;
+import com.cloud.SubwayChat.domain.User;
+import com.cloud.SubwayChat.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public User join(String nickName, SubwayLine line){
+        User user = new User(nickName, line);
+
+        return userRepository.save(user);
+    }
 }
