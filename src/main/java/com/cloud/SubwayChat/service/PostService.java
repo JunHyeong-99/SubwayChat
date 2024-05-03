ef2e7c54369aa3702ccaea865205853d1fdda2cd
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-05-03 11:26:48 +0900

    feat: PostService에 findPostList() 메서드 구현

--- src/main/java/com/cloud/SubwayChat/service/PostService.java
+++ src/main/java/com/cloud/SubwayChat/service/PostService.java
@@ -6,8 +6,13 @@ import com.cloud.SubwayChat.domain.User;
 import com.cloud.SubwayChat.repository.PostRepository;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -30,5 +35,12 @@ public class PostService {
                 .build();
 
         postRepository.save(post);
+    }
+
+    @Transactional
+    public Page<Post> findPostList(int pageNo){
+        Pageable pageable = PageRequest.of(pageNo, 10);
+        
+        return postRepository.findAll(pageable);
     }
 }
