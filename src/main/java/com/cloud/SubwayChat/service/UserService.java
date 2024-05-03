fe21e58a364a603a058befbd060be553ec80a581
hoyaii <131665728+hoyaii@users.noreply.github.com>
2024-05-03 11:03:30 +0900

    feat: 접속 시 nickName과 line이 세션에 저장되도록 하는 로직 추가

--- src/main/java/com/cloud/SubwayChat/service/UserService.java
+++ src/main/java/com/cloud/SubwayChat/service/UserService.java
@@ -3,6 +3,7 @@ package com.cloud.SubwayChat.service;
 import com.cloud.SubwayChat.domain.SubwayLine;
 import com.cloud.SubwayChat.domain.User;
 import com.cloud.SubwayChat.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,11 +16,15 @@ public class UserService {
     private final UserRepository userRepository;
 
     @Transactional
-    public User join(String nickName, SubwayLine line){
+    public User join(String nickName, SubwayLine line, HttpSession session){
         User user = User.builder()
                 .nickName(nickName)
                 .line(line)
                 .build();
+
+        // 세션에 사용자 정보 저장
+        session.setAttribute("USER_ID", user.getId());
+        session.setAttribute("NICKNAME", user.getNickName());
 
         return userRepository.save(user);
     }
