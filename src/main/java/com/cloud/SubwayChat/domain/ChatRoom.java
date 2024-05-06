50339a424ad9171aeb307dbdc1b67c6eb300793a
junhyeong Park <64734115+pnu201924480@users.noreply.github.com>
2024-05-07 01:28:49 +0900

    fix: 안쓰는 속성 제거

--- src/main/java/com/cloud/SubwayChat/domain/ChatRoom.java
+++ src/main/java/com/cloud/SubwayChat/domain/ChatRoom.java
@@ -1,7 +1,27 @@
 package com.cloud.SubwayChat.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChatRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true) // redis에 저장하기 위한 String roomId
+    private String roomId;
+
+    private SubwayLine subwayLine;
+
+    @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.REMOVE)
+    private List<Message> messageList = new ArrayList<>();
+    
 }
