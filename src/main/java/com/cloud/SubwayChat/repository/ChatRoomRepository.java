823c1dea85c75d934b9db53f1aab435ddcbc3da8
junhyeong Park <64734115+pnu201924480@users.noreply.github.com>
2024-05-07 01:27:45 +0900

    feat: existBySubwayLine 구현

--- src/main/java/com/cloud/SubwayChat/repository/ChatRoomRepository.java
+++ src/main/java/com/cloud/SubwayChat/repository/ChatRoomRepository.java
@@ -1,9 +1,11 @@
 package com.cloud.SubwayChat.repository;
 
 import com.cloud.SubwayChat.domain.ChatRoom;
+import com.cloud.SubwayChat.domain.SubwayLine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+    boolean existsBySubwayLine(SubwayLine subwayLine);
 }
