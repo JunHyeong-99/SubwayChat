1588df60d221c90634851ff193255c567d7bcebb
junhyeong Park <64734115+pnu201924480@users.noreply.github.com>
2024-05-12 17:19:16 +0900

    feat: findByRoomId 추가

--- src/main/java/com/cloud/SubwayChat/repository/ChatRoomRepository.java
+++ src/main/java/com/cloud/SubwayChat/repository/ChatRoomRepository.java
@@ -5,7 +5,10 @@ import com.cloud.SubwayChat.domain.SubwayLine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     boolean existsBySubwayLine(SubwayLine subwayLine);
+    Optional<ChatRoom> findByRoomId(String roomId);
 }
