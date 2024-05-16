c3e92c18538041fb0b0cdcc420e839232461c3c0
junhyeong Park <64734115+pnu201924480@users.noreply.github.com>
2024-05-16 21:57:34 +0900

    fix: chatDetail.html 사용하지 않는 class 제거

--- src/main/java/com/cloud/SubwayChat/repository/ChatUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cloud.SubwayChat.repository;
-
-import com.cloud.SubwayChat.domain.ChatUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChatUserRepository extends JpaRepository<ChatUser, Long> {
-}
