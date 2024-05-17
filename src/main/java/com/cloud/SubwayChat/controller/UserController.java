a31f673247f77a447bb84cc4bf3d6f180a8947c7
junhyeong Park <64734115+pnu201924480@users.noreply.github.com>
2024-05-17 17:32:12 +0900

    fix: 유저가 들어올 때 지정한 닉네임 사용해서 채팅방 입장하도록 수정

--- src/main/java/com/cloud/SubwayChat/controller/UserController.java
+++ src/main/java/com/cloud/SubwayChat/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.cloud.SubwayChat.controller;
 
 import com.cloud.SubwayChat.controller.dto.ChatRoomDto;
+import com.cloud.SubwayChat.controller.dto.UserDto;
 import com.cloud.SubwayChat.domain.SubwayLine;
 import com.cloud.SubwayChat.domain.User;
 import com.cloud.SubwayChat.service.ChatRoomService;
 import com.cloud.SubwayChat.service.UserService;
 import jakarta.servlet.http.HttpSession;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,16 +25,27 @@ public class UserController {
     private final ChatRoomService chatRoomService;
 
     @GetMapping("/")
-    public String createUserForm(Model model) {
+    public String createUserForm(Model model, HttpSession session) {
+        Long userId = (Long) session.getAttribute("USER_ID");
+        if (userId != null) {
+            return "redirect:/home";
+        }
+
         model.addAttribute("user", new User());
 
         return "createUser";
     }
 
     @PostMapping("/join")
-    public String join(@ModelAttribute User user, HttpSession session) {
+    public String join(@ModelAttribute User user, HttpSession session, Model model) {
         userService.join(user.getNickName(), session);
-
+        model.addAttribute("nickName", user.getNickName());
+        return "mainPage";
+    }
+    @GetMapping("/home")
+    public String home(Model model, HttpSession session) {
+        UserDto userDto = userService.findUser((Long) session.getAttribute("USER_ID"));
+        model.addAttribute("nickName", userDto.getNickName());
         return "mainPage";
     }
 }
