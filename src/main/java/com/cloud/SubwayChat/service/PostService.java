f6d53991ee7edc835b163fb34f57a636b27984be
junhyeong Park <64734115+pnu201924480@users.noreply.github.com>
2024-06-08 13:28:44 +0900

    fix: PostService 최근 게시물 먼저 조회되도록 수정

--- src/main/java/com/cloud/SubwayChat/service/PostService.java
+++ src/main/java/com/cloud/SubwayChat/service/PostService.java
@@ -15,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -46,7 +47,7 @@ public class PostService {
 
     @Transactional
     public PostsDto findPostList(int pageNo){
-        Pageable pageable = PageRequest.of(pageNo, 10);
+        Pageable pageable = PageRequest.of(pageNo, 10, Sort.by(Sort.Direction.DESC, "createdDate"));
         Page<Post> postsPage = postRepository.findAll(pageable);
         return PostsDto.toDto(postsPage, pageNo);
     }
